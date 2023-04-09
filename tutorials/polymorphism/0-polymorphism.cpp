015388f8993770f97c1ba3e495d5001c252ed75d
godcandidate <godcandidate@gmail.com>
2023-04-09 15:58:03 +0000

    create 0-polymorphism.cpp

---
+++ tutorials/polymorphism/0-polymorphism.cpp
@@ -0,0 +1,93 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Polygon
+{
+    protected:
+        float width;
+        float height;
+
+    public:
+
+        void setParams(float w, float h)
+        {
+            width = w;
+            height = h;
+        }
+
+        float getParams()
+        {
+            float params[2];
+            params[0] = width;
+            params[1] = height;
+            return width;
+        }
+
+        //Polymorphic method
+        virtual float area()
+        {
+            return 0;
+        }
+};
+
+class Square: public Polygon
+{
+    public:
+        Square()
+        {
+            height = 0;
+        }
+        float area()
+        {
+            return width * width;
+        }
+};
+
+class Rectangle: public Polygon
+{
+    public:
+        float area()
+        {
+            return height * width;
+        }
+};
+
+class Triangle: public Polygon
+{
+    public:
+        float area()
+        {
+            return height * width * 0.5;
+        }
+
+};
+
+int main()
+{
+    Square sq;
+    Rectangle rect;
+    Triangle tria;
+
+
+    Polygon *squareA = &sq;
+    Polygon *rectB = &rect;
+    Polygon *triC = &tria;
+
+    squareA->setParams(10, 0);
+    rectB->setParams(10, 5);
+    triC->setParams(10, 5);
+
+
+    cout << "Square    : " << squareA->getParams() << endl;
+    cout << "Rectangle : " << rectB->getParams()  << endl;
+    cout << "Triangle  : " << triC->getParams() << endl;
+
+
+    cout << squareA->area() << endl;
+    cout << rectB->area() << endl;
+    cout << triC->area() << endl;
+
+    return 0;
+}
